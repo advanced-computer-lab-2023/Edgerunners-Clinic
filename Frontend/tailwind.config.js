304e18ff57247165c0f57a2dd6717bb0320d4f32
Roaa <roaamahmoud53@gmail.com>
2023-10-05 23:56:49 +0300

    package.json resolved

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+

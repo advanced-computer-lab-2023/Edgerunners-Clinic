0357d34a5d51931d501818b84d5c579eeae95f45
Hazem Sherif <hazem.sh2002@hotmail.com>
2023-10-08 04:35:00 +0300

    fixed setadmin

--- Frontend/src/Components/Admin/SetAdmin.jsx
+++ Frontend/src/Components/Admin/SetAdmin.jsx
@@ -1,119 +1,113 @@
 import Card from "../../UI/UX/Card";
 import Logo from "../../UI/UX/Logo";
-import { useRef, useState } from "react";
+import { useRef,useState } from "react"; 
 import axios from "axios";
 import WarningCard from "../../UI/WarningCard";
 
-function SetAdmin(props) {
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-  const passwordConRef = useRef();
-  const [warning, setWarning] = useState("");
-  const [ok, setOk] = useState(false);
+function SetAdmin(props){
+    const usernameRef=useRef();
+    const passwordRef=useRef();
+    const passwordConRef=useRef();
+    const [warning,setWarning] = useState("");
+    const [ok,setOk]= useState(false); 
+    
+    function okHandeler(){
+        setOk(false);
+    }
 
-  function okHandeler() {
-    setOk(false);
-  }
+    function confirmHandeler(event){
+        const usernameValue= usernameRef.current.value;
+        const passwordValue= passwordRef.current.value;
+        
+        const newAdmin={
+            Username: usernameValue,
+            Password:passwordValue,
+        };
+        
+        if (!usernameValue) {
+            setWarning("Please enter your name");
+            setOk(true);
+        } else if (!passwordValue) {
+            setWarning("Please enter your password");
+            setOk(true);
+        }  else if ((!passwordConRef.current.value) || passwordConRef.current.value!==passwordValue) {
+            setWarning("Confirmed Password does not match your new password");
+            setOk(true);
+        }
+        else{
+            axios
+        .post("http://localhost:3001/addAdmin", newAdmin, {
 
-  function confirmHandeler(event) {
-    const usernameValue = usernameRef.current.value;
-    const passwordValue = passwordRef.current.value;
-
-    const newAdmin = {
-      Username: usernameValue,
-      Password: passwordValue,
-    };
-
-    if (!usernameValue) {
-      setWarning("Please enter your name");
-      setOk(true);
-    } else if (!passwordValue) {
-      setWarning("Please enter your password");
-      setOk(true);
-    } else if (
-      !passwordConRef.current.value ||
-      passwordConRef.current.value !== passwordValue
-    ) {
-      setWarning("Confirmed Password does not match your new password");
-      setOk(true);
-    } else {
-      axios
-        .post("http://localhost:3001/addAdmin", newAdmin, {})
+        })
         .then((res) => {
-          usernameRef.current.value = "";
-          passwordRef.current.value = "";
-          passwordConRef.current.value = "";
+         
+        
+          usernameRef.current.value="";
+          passwordRef.current.value="";
+          passwordConRef.current.value="";
+         
+
         })
         .catch((error) => {
-          setOk(true);
-          setWarning("username already exists");
+         
+            setOk(true);
+            setWarning("username already exists")
+          
         });
+        }
+
     }
-  }
 
-  function submitHandeler(event) {
-    event.preventDefault();
-  }
+    function submitHandeler(event){
+        event.preventDefault();
+       
 
-  return (
-    <div>
-      <div className=" justify-center flex mt-20">
-        <h1 className=" hazem-text-2xl hazem-font-bold  hazem-text-center  hazem-text-sky-600  hazem-ml-0   hazem-mt-6 ">
-          {" "}
-          New Admin{" "}
-        </h1>
-      </div>
-      <div className=" hazem-flex hazem-justify-center hazem-mt-7">
-        <form onSubmit={submitHandeler}>
-          <div className=" hazem-mt-3">
-            <div className=" hazem-mb-4">
-              <label className=" hazem-text-xl hazem-font-bold   hazem-font-SourceSansPro  hazem-text-gray-500 hazem-ml-2">
-                {" "}
-                Username :{" "}
-              </label>
-              <br />
-              <input
-                type="text"
-                id="username"
-                name="username"
-                ref={usernameRef}
-                className="hazem-w-full hazem-px-4 hazem-py-2 hazem-border hazem-rounded-md hazem-focus:outline-none hazem-focus:border-blue-500"
-              />
+       
+    }
+   
+
+return(
+    <div className=" justify-center flex mt-20">
+
+   
+    <Card width='w-4/12' height=' h-[32rem]'>
+        <div className=" flex justify-center  mt-6 mb-0 ">
+            <Logo height='4rem'/>
+           
+        <h1 className=" text-2xl font-bold  text-center  text-sky-600  ml-0   mt-6 ">  New Admin </h1>
+        </div >
+        <div className=" flex justify-center mt-7">
+            
+        <form  onSubmit={submitHandeler}>
+            <div className=" mt-3">
+            <div className=" mb-4">
+            <label className=" text-xl font-bold   font-SourceSansPro  text-gray-500 ml-2"> Username : </label>
+            <br />
+            <input type="text" id="username" name="username"  ref={usernameRef} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
             </div>
 
-            <div className=" hazem-mb-4">
-              <label className=" hazem-text-xl hazem-font-bold   hazem-font-SourceSansPro  hazem-text-gray-500  hazem-ml-2">
-                {" "}
-                Password :{" "}
-              </label>
-              <br />
-              <input
-                type="password"
-                id="password"
-                ref={passwordRef}
-                className="hazem-w-full hazem-px-4 hazem-py-2 hazem-border hazem-rounded-md hazem-focus:outline-none hazem-focus:border-blue-500"
-              />
+            <div className=" mb-4">
+            <label className=" text-xl font-bold   font-SourceSansPro  text-gray-500  ml-2"> Password : </label>
+            <br />
+            <input type="password" id="password"  ref={passwordRef} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
             </div>
-            <div className=" hazem-mb-4">
-              <label className=" hazem-text-xl hazem-font-bold    hazem-font-SourceSansPro   hazem-text-gray-500 hazem-ml-2">
-                {" "}
-                Confirm Password :{" "}
-              </label>
-              <br />
-
-              <button
-                className="  text-sky-600  outline  w-40  h-9  rounded-md   mt-5 shadow block"
-                type="submit"
-                onClick={confirmHandeler}
-              >
-                {" "}
-                Confirm{" "}
-              </button>
+            <div className=" mb-4">
+            <label className=" text-xl font-bold    font-SourceSansPro   text-gray-500 ml-2"> Confirm Password : </label>
+            <br />
+            <input type="password" id="passwordCon"  ref={passwordConRef} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
             </div>
-          </div>
+              <div className=" flex justify-center  mt-6">
+                <br />
+                <br />
+                <button className="  text-sky-600  outline  w-40  h-9  rounded-md   mt-5 shadow block" type="submit" onClick={confirmHandeler}> Confirm </button>
+             </div> 
+               
+            </div>
         </form>
-      </div>
+        </div>
+    </Card>
+    {ok && <WarningCard width='w-4/12' height=' h-[5rem]' onClick={okHandeler} >{warning}</WarningCard>}
     </div>
-  );
+)
 }
 export default SetAdmin;

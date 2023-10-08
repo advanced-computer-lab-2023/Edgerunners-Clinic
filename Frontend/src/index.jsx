de2de1bc3d39f5b5d6ec79de52a91ea5c5e6bf24
SeifEldinRefaat <seifrefaat23@gmail.com>
2023-10-08 21:37:43 +0300

    Prescriptions

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import PatientHome from "./Components/PatientHome/PatientHome";
+import Prescriptions from "./Components/PatientHome/Prescriptions";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PatientHome />} />
+        <Route path="/Prescriptions" element={<Prescriptions />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

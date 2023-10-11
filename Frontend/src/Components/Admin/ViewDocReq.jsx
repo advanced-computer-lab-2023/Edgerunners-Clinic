747b37bdf6ed0c952bcc8d470a3da4f34c06356c
Roaa <roaamahmoud53@gmail.com>
2023-10-11 19:28:28 +0300

    viewDoc

---
+++ Frontend/src/Components/Admin/ViewDocReq.jsx
@@ -0,0 +1,13 @@
+import Logo from "../../UI/UX/Logo";
+import Card from "../../UI/UX/Card";
+import axios from "axios";
+import { useState,UseRef } from "react";
+
+function ViewDocReq(props){
+    return(
+        <div className="tailwind">
+            
+        </div>
+    )
+}
+export default ViewDocReq;
--- Frontend/src/Components/DoctorHome/findDoctorFunc.js
+++ Frontend/src/Components/DoctorHome/findDoctorFunc.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function getDoctor() {
+export default function getPatient() {
   const [doctor, setDoctor] = useState([]);
   useEffect(() => {
-    getDoctor();
-    async function getDoctor() {
+    getPatient();
+    async function getPatient() {
       const x =  sessionStorage.getItem("Username");
-      const res = await axios.get("http://localhost:3001/findDoctor", {Username : {x}});
+      const res = await axios.get("http://localhost:3001/findDoctor", {
+        params: {
+          Username : x
+        }
+      });
       setDoctor(res.data);
     }
   }, []);
-  console.log(doctor);
   return doctor;
 }

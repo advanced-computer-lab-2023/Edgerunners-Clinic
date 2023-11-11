044c5b0197d57e6e5aa674c207d57a39c75f6712
Hazem Sherif <hazem.sh2002@hotmail.com>
2023-11-11 17:16:43 +0200

    fixed the navigation bar

--- Frontend/src/Components/Admin/AdminHome.jsx
+++ Frontend/src/Components/Admin/AdminHome.jsx
@@ -32,7 +32,7 @@ export default function AdminHome() {
                 <i className="fas fa-bars"></i>
               </span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarExample01">
+            <div className="navbar-collapse" id="navbarExample01">
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                   <a className="nav-link" aria-current="page" href="#pets">
--- Frontend/src/Components/Login_Reg/contract.jsx
+++ Frontend/src/Components/Login_Reg/contract.jsx
@@ -13,41 +13,94 @@ const ContractPage = () => {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="bg-white rounded-lg p-8 shadow-md w-96">
-        <h1 className="text-2xl font-semibold mb-4">Doctor's Contract</h1>
-        {accepted ? (
-          <div>
-            <p className="mb-4">Contract Accepted! You now have access to the home page.</p>
-          </div>
-        ) : rejected ? (
-          <div>
-            <p className="mb-4">Contract Rejected. You have been removed from the system.</p>
-          </div>
-        ) : (
-          <div>
-            <p className="mb-4">
-              This is the contract text. Please read it carefully. By accepting, you agree to the terms and conditions.
-            </p>
-            <div className="flex justify-between">
-              <button
-                className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded mr-2"
-                onClick={handleAccept}
-              >
-                Accept
-              </button>
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-                onClick={handleReject}
-              >
-                Reject
-              </button>
+    <div className='tailwind'>
+    <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+  Toggle modal
+</button>
+
+
+<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
             </div>
-          </div>
-        )}
-      </div>
+            
+            <div class="p-4 md:p-5 space-y-4">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+            
+            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
     </div>
+</div>
+</div>
   );
 };
 
+// <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+// <div className="bg-white rounded-lg p-8 shadow-md w-96">
+//   <h1 className="text-2xl font-semibold mb-4">Doctor's Contract</h1>
+//   {accepted ? (
+//     <div>
+//       <p className="mb-4">Contract Accepted! You now have access to the home page.</p>
+//     </div>
+//   ) : rejected ? (
+//     <div>
+//       <p className="mb-4">Contract Rejected. You have been removed from the system.</p>
+//     </div>
+//   ) : (
+//     <div id="medium-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+// <div class="relative w-full max-w-lg max-h-full">
+  
+//   <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      
+//       <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+//           <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+//               Default modal
+//           </h3>
+//           <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="medium-modal">
+//               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+//                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+//               </svg>
+//               <span class="sr-only">Close modal</span>
+//           </button>
+//       </div>
+//       <div class="p-4 md:p-5 space-y-4">
+//           <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+//               With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+//           </p>
+//           <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+//               The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+//           </p>
+//       </div>
+      
+//       <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+//           <button data-modal-hide="medium-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+//           <button data-modal-hide="medium-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+//       </div>
+//   </div>
+// </div>
+// </div>
+//   )}
+// </div>
+// </div>
 export default ContractPage;

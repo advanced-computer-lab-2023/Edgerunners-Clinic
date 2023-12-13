95bc5c5a844e3b27cfe01ce9115713becd8de8a8
YahiaEid <yahiae54@gmail.com>
2023-12-13 20:26:48 +0200

    req 47,48,49,66

--- Frontend/src/Components/PatientHome/Doctors.jsx
+++ Frontend/src/Components/PatientHome/Doctors.jsx
@@ -155,6 +155,7 @@ export default function Doctors() {
       Availability: "Reserved",
       PatientUsername: sessionStorage.getItem("Username"),
       NationalID: NationalID,
+      Status:"Upcoming",
     });
   };
 

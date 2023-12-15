6901538718754876f8d719f1c113e91c4e102256
Roaa837 <74208514+Roaa837@users.noreply.github.com>
2023-12-15 13:08:47 -0800

    addingpres
    creating pres

---
+++ Frontend/src/Components/DoctorHome/AddPrescription.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import Card from "../../UI/UX/Card";
+import Logo from "../../UI/UX/Logo";
+function AddPrescription(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default AddPrescription; 

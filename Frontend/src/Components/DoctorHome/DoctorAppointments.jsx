80851f8ce36a3763406f064593a967c54713a98a
Hazem Sherif <hazem.sh2002@hotmail.com>
2023-12-17 05:59:25 +0200

    ads

--- Frontend/src/Components/DoctorHome/DoctorAppointments.jsx
+++ Frontend/src/Components/DoctorHome/DoctorAppointments.jsx
@@ -145,7 +145,7 @@ export default function DoctorAppointments() {
               <div className="navbar-collapse" id="navbarExample01">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="#pets">
+                    <a className="nav-link" aria-current="page" href="/videocall">
                       Video Call
                     </a>
                   </li>
@@ -153,7 +153,7 @@ export default function DoctorAppointments() {
                     <a
                       className="nav-link"
                       aria-current="page"
-                      href="#adoptions"
+                      href="/chatd"
                     >
                       Chat
                     </a>
--- Frontend/src/Components/DoctorHome/StartACall.jsx
+++ Frontend/src/Components/DoctorHome/StartACall.jsx
@@ -44,7 +44,125 @@ function StartACall() {
 
 
   return (
-    <div>
+    <div className="Bootstrap PatientHome">
+        <div className="header">
+          <nav className="navbar navbar-expand-lg fixed-top navbar-scroll nav-color-bg">
+            <div className="container">
+              <a href="/DoctorHome">
+                <Logo />
+              </a>
+
+              <button
+                className="navbar-toggler ps-0"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarExample01"
+                aria-controls="navbarExample01"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon d-flex justify-content-start align-items-center">
+                  <i className="fas fa-bars"></i>
+                </span>
+              </button>
+              <div className="navbar-collapse" id="navbarExample01">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="/videocall">
+                      Video Call
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      href="/chatd"
+                    >
+                      Chat
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      href="/DoctorAppointments"
+                    >
+                      My Appointments
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      href=""
+                    >
+                      My Subscriptions
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      href="/Prescriptions"
+                    >
+                      Prescriptions
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      onClick={() => setWalletModal(true)}
+                    >
+                      My Wallet
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      href="/changePassword"
+                    >
+                      Change password
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      aria-current="page"
+                      onClick={() => {
+                        sessionStorage.removeItem("Username");
+                        sessionStorage.removeItem("type");
+                        sessionStorage.removeItem("token");
+                        window.location.replace("/");
+                      }}
+                    >
+                      Log Out
+                    </a>
+                  </li>
+                </ul>
+
+                <ul className="navbar-nav flex-row">
+                  <li className="nav-item">
+                    <a className="nav-link px-2" href="#!">
+                      <i className="fab fa-facebook-square"></i>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link px-2" href="#!">
+                      <i className="fab fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link ps-2" href="#!">
+                      <i className="fab fa-youtube"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
       <h1>Choose Your Patient</h1>
       {!isCallStarted ? (
         <form onSubmit={handleFormSubmit}>
